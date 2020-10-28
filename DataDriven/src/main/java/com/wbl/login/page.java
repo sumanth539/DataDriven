4086c387bddbe4a3d291b206aacc7f47a15df697
sumanth539 <70626977+sumanth539@users.noreply.github.com>
2020-10-28 09:59:42 +0530

    Update page.java

--- DataDriven/src/main/java/com/wbl/login/page.java
+++ DataDriven/src/main/java/com/wbl/login/page.java
@@ -12,29 +12,25 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class page extends Inistilization {
+public class page extends ExclUtil {
 	public static WebDriver driver;
-	XSSFWorkbook workbook;
-	XSSFSheet sheet;
-	XSSFCell cell;
+	
 
-	@Test
-	public void ReadData() throws IOException, Exception {
-		File src = new File("C:\\Users\\kanpuri\\testData.xls");
-		FileInputStream fil = new FileInputStream(src);
-		workbook = new XSSFWorkbook(fil);
-		sheet = workbook.getSheet("Sheet1");
-		for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-			cell = sheet.getRow(i).getCell(0);
-			driver.findElement(By.xpath("//input[@type='number']")).sendKeys(cell.getStringCellValue());
-			driver.findElement(By.xpath("//button[@class='btn btn-primary mb-5 float-right mr-5 mt-2']")).click();
-			FileOutputStream f = new FileOutputStream(src);
-			String message = "Pass";
-			sheet.getRow(i).createCell(2).setCellValue(message);
-			workbook.write(f);
-			f.close();
+    @FindBy(xpath="//input[@type='number']") private WebElement  number;
+    @FindBy(xpath="//button[@class='btn btn-primary mb-5 float-right mr-5 mt-2']")
+	private WebElement login;
 
-		}
-	}
+public  page(WebDriver driver) {
+	this.driver = driver;
+	PageFactory.initElements(driver, this);
+}
+
+public void send() {
+	number.sendKeys(getCellData());
+}
+
+public void loginButton() {
+	login.click();
+}
 
 }
